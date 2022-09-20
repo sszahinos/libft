b893ee524f5014088b25e675cf037a039bfd2aca
Sergio Sanchez Zahinos <sergiosaza6@gmail.com>
2022-09-20 13:13:54 +0200

    Functions updated

--- c1/Libft/ft_putnbr_fd.c
+++ c1/Libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: sersanch <sersanch@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/17 19:24:23 by sersanch          #+#    #+#             */
-/*   Updated: 2022/09/20 12:21:53 by sersanch         ###   ########.fr       */
+/*   Updated: 2022/09/20 13:13:52 by sersanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,24 +43,23 @@ void	ft_putnbr_fd(int n, int fd)
 {
 	int	i;
 	int digits;
-
+	
 	if (n < 0)
 	{
-		ft_puchar_fd('-', fd);
+		ft_putchar_fd('-', fd);
 		n *= -1;
 	}
-	else if (n == 0)
+	if (n == 0)
 		ft_putchar_fd('0', fd);
-	else
+	if 
+	else if (n <
 	{
-		digits = count_digits(n);
+		digits = count_digits(n) - 1;
 		i = 0;
 		while (i < digits)
 		{
-			ft_putchar_fd(n / pow(), fd);
+			ft_putchar_fd((n / pow(10, digits--)) + 48, fd);
 		}
+		ft_putchar_fd((n % 10) + 48, fd);
 	}
-
-
-	ft_putchar_fd((char)n + 48, fd);
 }
