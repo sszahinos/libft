c30fdb348837c124bb7b8815a00a1886d90ca818
Sergio Sanchez Zahinos <sergiosaza6@gmail.com>
2022-09-20 16:45:02 +0200

    Functions updated

--- c1/Libft/ft_itoa.c
+++ c1/Libft/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: sersanch <sersanch@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 21:53:46 by sersanch          #+#    #+#             */
-/*   Updated: 2022/09/20 15:09:03 by sersanch         ###   ########.fr       */
+/*   Updated: 2022/09/20 16:43:53 by sersanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ static int	get_len(int n)
 {
 	int	i;
 	int	sign;
-
+	
+	if (n == 2147483648)
+		return (11);
 	i = 0;
 	sign = 0;
 	if (n < 0)
@@ -34,14 +36,33 @@ static int	get_len(int n)
 	return (i + sign);
 }
 
+static char	*get_min(char *num)
+{
+	char	*min;
+	int		i;
+
+	min = "-2147483648";
+	i = 0;
+	while (i < 12)
+	{
+		num[i] = min[i];
+		i++;
+	}
+	num[i] = '\0';
+	return (num);
+}
+
 char	*ft_itoa(int n)
 {
 	char	*num;
 	int		i;
+
 	i = get_len(n) - 1;
 	num = malloc(sizeof(char) * (get_len(n) + 1));
 	if (!num)
 		return (0);
+	if (n == -2147483648LL)
+		return (get_min(num));
 	num[i + 1] = '\0';
 	if (n < 0)
 	{
