e058f8eceae2bcf6a17b90c88e11605ec1c8fd91
sszahinos <sergiosaza6@gmail.com>
2022-09-22 12:51:38 +0200

    Functions updated

---
+++ c1/Libft/ft_lstnew.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sersanch <sersanch@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/17 19:29:48 by sersanch          #+#    #+#             */
+/*   Updated: 2022/09/19 10:35:31 by sersanch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	lst;
+
+	lst = (t_list)malloc(sizeof(t_list));
+	if (!lst)
+		return (0);
+	lst.content = content;
+	lst.next = 0;
+}
