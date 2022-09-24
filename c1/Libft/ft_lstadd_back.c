dcd2529995d0ca1ade1d7f39e13f95eed877898f
Sergio Sanchez Zahinos <sergiosaza6@gmail.com>
2022-09-24 15:03:38 +0200

    lstadd_back fixed

--- c1/Libft/ft_lstadd_back.c
+++ c1/Libft/ft_lstadd_back.c
@@ -6,16 +6,23 @@
 /*   By: sersanch <sersanch@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/24 10:26:38 by sersanch          #+#    #+#             */
-/*   Updated: 2022/09/24 10:48:05 by sersanch         ###   ########.fr       */
+/*   Updated: 2022/09/24 15:02:54 by sersanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-void ft_lstadd_back(t_list **lst, t_list *new)
+#include <stdio.h>
+void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last;
 
-	last = ft_lstlast(*lst);
-	last->next = new;
+	if (!new)
+		return ;
+	if (!*lst)
+		*lst = new;
+	else
+	{
+		last = ft_lstlast(*lst);
+		last->next = new;
+	}
 }
