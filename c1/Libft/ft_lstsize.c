a4f82f2f5eaab8f2412f7a8328909f16ce89d100
Sergio Sanchez Zahinos <sergiosaza6@gmail.com>
2022-09-24 14:42:22 +0200

    lstsize fixed

--- c1/Libft/ft_lstsize.c
+++ c1/Libft/ft_lstsize.c
@@ -6,19 +6,21 @@
 /*   By: sersanch <sersanch@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/24 10:29:25 by sersanch          #+#    #+#             */
-/*   Updated: 2022/09/24 10:29:47 by sersanch         ###   ########.fr       */
+/*   Updated: 2022/09/24 14:41:51 by sersanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+#include <stdio.h>
 int	ft_lstsize(t_list *lst)
 {
 	int		count;
 	t_list	*aux;
 
 	aux = lst;
-	count = 0;
+	if (!lst)
+		return (0);
+	count = 1;
 	while (aux->next)
 	{
 		aux = aux->next;
