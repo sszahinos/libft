9cf95b7ebdc9295f2375007944416c25a0548334
Sergio Sanchez Zahinos <sergiosaza6@gmail.com>
2022-09-24 14:10:42 +0200

    substr fixed

--- c1/Libft/ft_substr.c
+++ c1/Libft/ft_substr.c
@@ -6,11 +6,41 @@
 /*   By: sersanch <sersanch@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/17 17:52:34 by sersanch          #+#    #+#             */
-/*   Updated: 2022/09/24 11:36:18 by sersanch         ###   ########.fr       */
+/*   Updated: 2022/09/24 14:01:50 by sersanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*static int	get_len(char const *s, unsigned int start, size_t len)
+{
+	int	entire_len;
+
+	entire_len = ft_strlen(s);
+	if ((long long int)len > 2147483647LL)
+		len = (size_t)((unsigned int)entire_len - start);
+	else if (start >= (unsigned int)entire_len)
+		len = 0;
+	else if (entire_len < len)
+		len = entire_len;
+	else if (entire_len < start + len)
+		len = entire_len - start;
+	return (len);
+}*/
+
+static int	get_len(char const *s, unsigned int start, size_t len)
+{
+	int	entire_len;
+
+	entire_len = ft_strlen(s);
+	if ((long long int)len > 2147483647LL)
+		len = (size_t)((unsigned int)entire_len - start);
+	else if (start >= (unsigned int)entire_len) //empieza mas alla
+		len = 0;
+	else if (start + len > entire_len) //empieza ok pero se pasa
+		len = entire_len - start;
+	return (len);
+}
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -22,10 +52,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		start = ft_strlen(s);
 		len = 0;
 	}
-	else if (start >= (unsigned int)ft_strlen(s))
-		len = 0;
-	else if (ft_strlen(s) < len)
-		len = ft_strlen(s);
+	else
+		len = get_len(s, start, len);
 	substr = malloc(sizeof(char) * (len + 1));
 	if (!substr)
 		return (0);
