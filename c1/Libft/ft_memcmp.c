19dc40ea873c100f0d680ca7511ca356f9b77358
Sergio Sanchez Zahinos <sergiosaza6@gmail.com>
2022-09-24 12:37:17 +0200

    Functions updated

--- c1/Libft/ft_memcmp.c
+++ c1/Libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: sersanch <sersanch@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 13:34:53 by sersanch          #+#    #+#             */
-/*   Updated: 2022/09/24 12:23:31 by sersanch         ###   ########.fr       */
+/*   Updated: 2022/09/24 12:36:12 by sersanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,23 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
+	int				ret;
 	size_t			i;
 	unsigned char	*s1c;
 	unsigned char	*s2c;
 
 	s1c = (unsigned char *)s1;
 	s2c = (unsigned char *)s2;
+	ret = 0;
 	i = 0;
 	while (i < n)
 	{
 		if ((unsigned char)s1c[i] > (unsigned char)s2c[i])
-			return (1);
-		if ((unsigned char)s1c[i] < (unsigned char)s2c[i])
-			return (-1);
+			ret = 1;
+		else if ((unsigned char)s1c[i] < (unsigned char)s2c[i])
+			ret = 1;
+		if (ret)
+			return ((unsigned char)s1c[i] - (unsigned char)s2c[i]);
 		i++;
 	}
 	return (0);
